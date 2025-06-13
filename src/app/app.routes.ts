db95c36fc46b04c2a09dcc77743aca1d31afc993
6iller <a.sabitov.msu.moskau@gmail.com>
2025-06-13 12:40:10 +0300

    v0.1 routes_change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CardsComponent } from './pages/cards/cards.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'cards', pathMatch: 'full' },
+  { path: 'cards', component: CardsComponent },
+  // можно добавить другие страницы
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
